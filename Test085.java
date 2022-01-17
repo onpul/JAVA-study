b431334d659c2b03167e14c455d818ac101f33c6
onpul <chmj072@gmail.com>
2022-01-17 23:47:46 +0900

    20220117 과제

--- Test085.java
+++ Test085.java
@@ -14,10 +14,62 @@
 // >> 가장 큰 수 → 91
 // 계속하려면 아무 키나 누르세요...
 //-------------------------------------------------------------------------------------------------
+import java.util.Scanner;
+import java.io.IOException;
+
 public class Test085
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
+		// 스캐너 인스턴스 생성
+		Scanner sc = new Scanner(System.in); // 공백 구분이니까 스캐너로 입력 받기
+
+		// 변수, 배열 선언
+		int num; // 사용자가 입력할 데이터의 갯수
+		int[] arr; // 배열
+		int max=0; // 가장 큰 수
+	
+		// 1. 사용자에게 입력 받아 배열 길이 설정
+		System.out.print("입력할 데이터의 갯수   : ");
+		num=sc.nextInt();
+		// System.out.print(num); // (테스트)
+
+		arr = new int[num]; // 사용자가 입력한 갯수만큼 배열 길이 설정
+		// System.out.print(arr.length); // (테스트)
+
+		// 2. 갯수만큼의 데이터 입력 받기, 배열에 저장
+		System.out.print("데이터 입력(공백 구분) : ");
 		
+		for (int i=0; i<num; i++) // i는 0부터 데이터 갯수만큼(0부터니까 포함X)
+		{
+			arr[i]=sc.nextInt();
+		}
+	
+		// 배열 출력 테스트
+		/*
+		for (int i=0; i<arr.length; i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		*/
+		
+		// 3. 가장 큰 수 찾기
+		for (int i=0; i<num; i++) // 배열 길이만큼 반복
+		{
+			if (arr[i] > max) // 배열 순서대로 max값에 들어가며 비교
+			{
+				max = arr[i];
+			}
+		}
+		System.out.println(">> 가장 큰 수 → " + max);
 	}
 }
+//-------------------------------------------------------------------------------------------------
+// 실행 결과
+/*
+입력할 데이터의 갯수   : 12
+데이터 입력(공백 구분) : 74 65 13 91 5 67 33 41 2 50 11 38
+>> 가장 큰 수 → 91
+계속하려면 아무 키나 누르십시오 . . .
+*/
--- Test086.java
+++ Test086.java
@@ -28,10 +28,85 @@
 //   98 : -24.0
 //   계속하려면 아무 키나. . .
 //-------------------------------------------------------------------------------------------------
+import java.util.Scanner;
+import java.io.IOException;
+
 public class Test086
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
+		// 스캐너 인스턴스 생성
+		Scanner sc = new Scanner(System.in);
 		
+		// 변수 선언
+		int num; // 학생 수
+		int[] arr; // 배열
+		int sum=0; // 합계
+		double aver, devi; // 평균, 편차
+		aver=devi=0;
+
+		// 사용자로부터 학생 수 입력받기
+		System.out.print("학생 수 입력 : ");
+		num = sc.nextInt();
+
+		// 학생 수만큼 배열 길이 설정
+		arr = new int[num];
+
+		// 배열 길이만큼(학생 수만큼 점수 입력받기, 배열에 저장)
+		for (int i=0; i<num; i++)
+		{
+			System.out.printf("%d번 학생의 점수 입력 : ", (i+1));
+			arr[i] = sc.nextInt();
+		}		
+
+		// 테스트 (배열 전체 출력)
+		/*
+		for (int i=0; i<arr.length; i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+		*/
+
+		// 합계, 평균 구하기
+		for (int i=0; i<arr.length; i++)
+		{
+			sum+=arr[i];
+			aver=sum/arr.length;
+		}
+		// 테스트 
+		//System.out.println("합계: " + sum);
+		//System.out.println("평균: " + aver);
+
+		// 출력
+		System.out.printf(">> 합 : %d%n", sum);
+		System.out.printf(">> 평균 : %.1f%n", aver);
+		System.out.println(">> 편차 : ");
+
+
+		// 편차 구하기 + 출력
+		for (int i=0; i<arr.length; i++)
+		{
+			devi = aver-arr[i];
+			System.out.printf("%d : %.1f%n", arr[i], devi);
+		}
 	}
 }
+//-------------------------------------------------------------------------------------------------
+// 실행 결과
+/*
+학생 수 입력 : 5
+1번 학생의 점수 입력 : 90
+2번 학생의 점수 입력 : 82
+3번 학생의 점수 입력 : 64
+4번 학생의 점수 입력 : 36
+5번 학생의 점수 입력 : 98
+>> 합 : 370
+>> 평균 : 74.0
+>> 편차 :
+90 : -16.0
+82 : -8.0
+64 : 10.0
+36 : 38.0
+98 : -24.0
+계속하려면 아무 키나 누르십시오 . . .
+*/
